8d0645ee69eeeb0f7b71682f637b546b677a4550
Tien M. Pham <tienpmuit@gmail.com>
2023-05-25 16:36:07 +0700

    Update solution for codeforces 874 Div 3

---
+++ codeforces/1833-874D3/F.cpp
@@ -0,0 +1,27 @@
+/*
+ * 	author: Tien M. Pham
+ * 	created: 25.05.2023
+ * 	description: https://codeforces.com/contest/1833/problem/F
+ * 	status: N/A
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+	
+}
+
+int main() {
+	//ios_base::sync_with_stdio(false);
+	// cin.tie(nullptr);
+	
+	int T;
+	cin >> T;
+	while (T--) {
+		solve();
+	}
+
+	return 0;
+}
